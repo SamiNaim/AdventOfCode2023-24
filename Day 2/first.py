f337e31c1beaa24e736661320afd81b9d57e6d3b
SamiNaim <sami.naim4841@gmail.com>
2023-12-02 22:08:55 +0100

    DONE: Day 2

---
+++ Day 2/first.py
@@ -0,0 +1,32 @@
+inputFile = open("C:\\Users\\User\\Desktop\\Projects\\AdventOfCode2023-24\\AdventOfCode2023-24\\Day 2\\input.txt", "r")
+lines = inputFile.readlines()
+
+possibleGames = 0
+
+for line in lines:
+    possible = True
+    
+    splitString = line.split(": ")
+    splitId = int(splitString[0].split(" ")[1])
+    
+    splitCubes = splitString[1].split("; ")
+    for cube in splitCubes:
+        splitColors = cube.split(", ")
+        for color in splitColors:
+            splitNumber = color.split(" ")
+            if splitNumber[1].rstrip() == "red" and int(splitNumber[0]) > 12:
+                possible = False
+                break
+            elif splitNumber[1].rstrip() == "green" and int(splitNumber[0]) > 13:
+                possible = False
+                break
+            elif splitNumber[1].rstrip() == "blue" and int(splitNumber[0]) > 14:
+                possible = False
+                break
+        if not possible:
+            break
+    
+    if possible:
+        possibleGames += splitId
+
+print(possibleGames)
---
+++ Day 2/second.py
@@ -0,0 +1,28 @@
+inputFile = open("C:\\Users\\User\\Desktop\\Projects\\AdventOfCode2023-24\\AdventOfCode2023-24\\Day 2\\input.txt", "r")
+lines = inputFile.readlines()
+
+sumPowerSets = 0
+
+for line in lines:
+    red = 0
+    green = 0
+    blue = 0
+    
+    splitString = line.split(": ")
+    splitId = int(splitString[0].split(" ")[1])
+    
+    splitCubes = splitString[1].split("; ")
+    for cube in splitCubes:
+        splitColors = cube.split(", ")
+        for color in splitColors:
+            splitNumber = color.split(" ")
+            if splitNumber[1].rstrip() == "red":
+                red = max(int(splitNumber[0]), red)
+            elif splitNumber[1].rstrip() == "green":
+                green = max(int(splitNumber[0]), green)
+            elif splitNumber[1].rstrip() == "blue":
+                blue= max(int(splitNumber[0]), blue)
+    
+    sumPowerSets += red * green * blue
+
+print(sumPowerSets)
